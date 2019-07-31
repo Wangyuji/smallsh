c942fd217f6eb96b19e4555c0395c2401264a768
Matthew Solbrack <solbracm@os1.engr.oregonstate.edu>
2019-07-31 09:12:24 -0700

    Skeleton of smallsh.c file

---
+++ smallsh.c
@@ -0,0 +1,15 @@
+/**************************************************************************************
+ * Author: Matthew Solbrack
+ * Date: 7/30/2019
+ * Program: smallsh (Program 3 - CS 344)
+ * Description: This is my own shell in C, similar to bash. The shell will run command
+ * line instructions and return the results similar to other shells. The shell has 
+ * 3 built in commands: exit, cd, and status. It will also support comments. 
+ * ***********************************************************************************/
+
+main()
+{
+
+
+	return 0;
+}
