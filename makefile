fcf8198a32df098b2a761a4ba8945fb540d11a20
Matthew Solbrack <solbracm@os1.engr.oregonstate.edu>
2019-07-31 12:51:44 -0700

    added exit feature and command line string feature

---
+++ makefile
@@ -0,0 +1,8 @@
+make:	smallsh.c
+	gcc -o smallsh smallsh.c -Wall
+	./smallsh
+
+edit:	smallsh.c
+	vim smallsh.c
+
+
--- smallsh.c
+++ smallsh.c
@@ -6,10 +6,31 @@
  * line instructions and return the results similar to other shells. The shell has 
  * 3 built in commands: exit, cd, and status. It will also support comments. 
  * ***********************************************************************************/
+#include <stdio.h> //print and scanf libraries
+#include <string.h> //string usage
+#include <stdlib.h>
+#define MAX 100
 
-main()
+
+int main()
 {
+	while(1)
+	{
+	 	char str[100]; //Capture the string entered by the user
 
+		// Making it look like a terminal; ":" is used as a prompt for each command line
+		fflush(stdin);	//flush the output buffer 
+		printf(": ");
+		fgets(str, MAX, stdin);
+		str[strlen(str)-1] = '\0'; //remove the newline from the string
+		
+
+		// exit when the user enters "exit $"
+		if(strcmp(str, "exit $") == 0) { exit(0); } 
+
+
+
+	}
 
 	return 0;
 }
